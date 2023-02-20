a52e1f461bd21be385e32ecb0e768670ef95c552
shahnaz hr <shahnazhasyar@gmail.com>
2023-02-20 22:17:56 +0700

    first commit

---
+++ html/assets/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Toggle navigation
+
+function openNav() {
+   	document.getElementById('navigation').style.display = 'block';
+  }
+function closeNav() {
+   	document.getElementById('navigation').style.display = 'none';
+  }
